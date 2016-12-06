a249b8771dc3b567c04be6585dbabafc58a9ea63
Elijah <elieel@gmail.com>
2016-12-06 14:35:56 -0500

    Complete basic express exercises

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.use(bodyParser.json())
+
+app.get('/name/:name/age/:age', function(req, res) {
+  var age = req.params.age;
+  var year = new Date().getFullYear();
+  var name = req.params.name;
+  var bYear = year - age;
+  res.render('hello.hbs', {
+    title: 'Hello',
+    name: name,
+    year: bYear
+  });
+  // res.json({
+  //   message: 'Hello ' + name + '!\nYou were born in ' + bYear + '!'
+  // });
+});
+
+app.listen(1337, function() {
+  console.log("1337");
+});
